cc02e6ec75046fcc4d69a7a3a6df4df974f015d7
KobzevR <kobzeff.rom@yandex.ru>
2017-04-28 23:31:52 +0300

    Test join groups

---
+++ Test join groups.cpp
@@ -0,0 +1,23 @@
+void TestJoinGroups()
+{
+	char testgroup1[20] = "1.txt";
+	char testgroup2[20] = "61.txt";
+	char testnewgroup[20] = "61new.txt";
+	ifstream fin1(testgroup1);
+	ifstream fin2(testgroup2);
+	ofstream fout(testnewgroup, ios_base::ate);
+	if (!fin1.is_open() && !fin2.is_open())
+		cout << "Testing failed!"
+	else
+	{
+		const int max_length1 = 1024;
+		char str1[max_length1];
+		fin1.getline(str1, max_length1);
+		fout << str1;
+		const int max_length2 = 1024;
+		char str2[max_length2];
+		fin2.getline(str2, max_length2);
+		fout << str2;
+		cout << "Testing succeeded!"
+	}
+}
